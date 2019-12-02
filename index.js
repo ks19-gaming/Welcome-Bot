d078ae6dd10022a78048220e408a088bcfa275bf
Glitch (deciduous-lily) <none>
2019-12-02 13:40:02 +0000

    🎻🌛 Checkpoint ./index.js:7865323/331 ./package.json:7865323/836

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 60000);
+
 const Discord = require("discord.js")
 const config = require("./config.json")
 const bot = new Discord.Client();
